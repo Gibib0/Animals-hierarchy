4ad233b91f477510f618445a0585ff280bf25d43
Anastasia <anast111antonova@gmail.com>
2025-09-28 22:25:34 +0300

    Add classes checking

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ class Mammals extends Animal {
 	constructor(name, age, weight, limbAmount) {
 		super(name, age, weight, limbAmount)
 	}
+
+	static isMammal(obj) {
+		return obj instanceof Mammals
+	}
 }
 
 class Birds extends Animal {
@@ -55,7 +59,7 @@ class Birds extends Animal {
 }
 
 class Fish extends Animal {
-	constructor(name, age, weight, limbAmount) {
+	constructor(name, age, weight, limbAmount = 0) {
 		super(name, age, weight, limbAmount)
 	}
 
@@ -124,6 +128,10 @@ class Dog extends Predators {
 		console.log(`${this.name} runs`);
 	}
 
+	say() {
+		console.log(`${this.name} barks`);
+	}
+
 	eat() {
 		console.log(`${this.name} eats bone`);
 	}
@@ -152,7 +160,7 @@ class Human extends Primates {
 		super(name, age, weight, limbAmount);
 	}
 
-	static isHuamn(obj) {
+	static isHuman(obj) {
 		return obj instanceof Human
 	}
 
@@ -168,3 +176,40 @@ class Human extends Primates {
 		console.log(`${this.name} eats fastfood`);
 	}
 }
+
+const dog1 = new Dog('Sharik', 5, 5)
+const dog2 = new Dog('Barbos', 11, 25, 3)
+
+const dolphin1 = new Dolphin('Flipper', 10, 150, 0)
+const dolphin2 = new Dolphin('Goody', 3, 80)
+
+const human1 = new Human('John', 30, 78, 4)
+const human2 = new Human('Maria', 24, 60, 4)
+
+const animal1 = new Animal('Cat', 2, 3, 4)
+const animal2 = new Animal('Hippo', 13, 90, 4)
+
+const bird1 = new Birds('jack', 2, 0.5, 4)
+const bird2 = new Birds('black', 10, 10, 4)
+
+
+dog1.move()
+dog2.say()
+dog1.eat()
+
+dolphin1.say()
+dolphin1.move()
+
+human1.move()
+human1.say()
+
+console.log(dog1.age);
+console.log(dolphin1.weight);
+console.log(human1.limbAmount);
+
+console.log(Animal.isAnimal(dog1));
+console.log(Mammals.isMammal(dolphin1));
+console.log(Dog.isDog(dolphin2));
+console.log(Human.isHuman(dog2));
+console.log(Dolphin.isDolphin(dog2));
+console.log(Birds.isBird(bird1));
